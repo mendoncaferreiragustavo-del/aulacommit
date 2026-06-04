2afeba6c70e7a69e69f0a5db4f7ca45e5c86e5ab
mendoncaferreiragustavo-del <mendoncaferreiragustavo@gmail.com>
2026-06-03 22:33:23 -0300

    codigo

--- aula_commit.py
+++ aula_commit.py
@@ -1,1 +1,19 @@
 # LÓGICA DE PROGRAMAÇÃO EM PYTHON
+# 1. Variáveis
+nome = input("Gustavo Mendonça")
+idade = int(input("Digite sua idade" \
+"25"))
+
+# 2. Exibição de informações
+print("\nOlá,", nome)
+print("Sua idade é:", idade)
+
+# 3. Estrutura condicional
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+# 4. Operações matemáticas
+numero1 = float(input("\nDigite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
